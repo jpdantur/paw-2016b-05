acb6411c61686cee6d6d40a1f625278472385b98
Martin Goffan <martin.goffan@me.com>
2016-10-30 22:36:48 -0300

    Bug fixes

--- model/src/main/java/edu/tp/paw/model/StoreItem.java
+++ model/src/main/java/edu/tp/paw/model/StoreItem.java
@@ -152,27 +152,31 @@ public class StoreItem {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
-
-
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (!(obj instanceof StoreItem)) {
 			return false;
+		}
 		StoreItem other = (StoreItem) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
 			return false;
+		}
 		return true;
 	}
-
-
 
 	public User getOwner() {
 		return owner;
