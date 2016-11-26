b8216b01b8ebdce237e765f19b8f69827cd64e2e
Martin Goffan <martin.goffan@me.com>
2016-11-26 16:22:49 -0300

    Some profile stuff

--- webapp/src/main/java/edu/tp/paw/webapp/controller/IdController.java
+++ webapp/src/main/java/edu/tp/paw/webapp/controller/IdController.java
@@ -3,12 +3,17 @@ package edu.tp.paw.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.tp.paw.interfaces.service.IUserService;
 import edu.tp.paw.model.User;
+import edu.tp.paw.model.filter.Filter;
+import edu.tp.paw.model.filter.FilterBuilder;
+import edu.tp.paw.model.filter.StoreItemStatusFilter.ItemStatusFilter;
 import edu.tp.paw.webapp.exceptions.StoreItemNotFoundException;
+import edu.tp.paw.webapp.form.ProfileItemSearchForm;
 
 @Controller
 @RequestMapping("/id")
@@ -18,6 +23,7 @@ public class IdController extends BaseController {
 	
 	@RequestMapping( value = {"/{username}", "/{username}/buyer"})
 	public String userProfileBuyer(
+			@ModelAttribute("profileItemSearch") final ProfileItemSearchForm form,
 			@PathVariable("username") final String username,
 			final Model model
 		) {
@@ -28,6 +34,22 @@ public class IdController extends BaseController {
 			throw new StoreItemNotFoundException();
 		}
 		
+		final Filter filter =
+				FilterBuilder
+				.create()
+				.query()
+					.text(form.getQuery())
+				.and().status()
+					.status(ItemStatusFilter.ACTIVE)
+				.and().page()
+					.size(form.getPageSize())
+					.take(form.getPageNumber())
+				.and().sort()
+					.by(form.orderBy())
+					.order(form.sortOrder())
+				.end().build();
+		
+		model.addAttribute("items", userService.getPublishedItems(user, filter));
 		model.addAttribute("user", user);
 		
 		return "id";
