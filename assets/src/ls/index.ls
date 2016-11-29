4a734077c0ce556d79bfdfba37ab7ba7119d89ca
Martin Goffan <martin.goffan@me.com>
2016-11-29 20:29:34 -0300

    Removed home logo animation on scroll

--- assets/src/ls/index.ls
+++ assets/src/ls/index.ls
@@ -4,21 +4,6 @@ $ document .ready !->
 	$window = $ window
 	$navbar = $ \#navbar-content
 
-
-	$window .scroll (e) !->
-
-		if !$navbar.hasClass(\scrolled) && $navbar.offset!.top > 50
-			$navbar.addClass \scrolled
-
-			$ \.navbar-brand .animate do
-				'font-size': '1em'
-
-		else if $navbar.hasClass(\scrolled) && $navbar.offset!.top <= 50
-			$navbar .removeClass \scrolled
-
-			$ \.navbar-brand .animate do
-				'font-size': '2em'
-
 	$ \#simple-search-form .submit (e) !->
 
 		if $ .trim($ \#simple-search-input .val!) == ''
--- webapp/src/main/webapp/resources/js/index.js
+++ webapp/src/main/webapp/resources/js/index.js
@@ -2,19 +2,6 @@ $(document).ready(function(){
   var $window, $navbar;
   $window = $(window);
   $navbar = $('#navbar-content');
-  $window.scroll(function(e){
-    if (!$navbar.hasClass('scrolled') && $navbar.offset().top > 50) {
-      $navbar.addClass('scrolled');
-      $('.navbar-brand').animate({
-        'font-size': '1em'
-      });
-    } else if ($navbar.hasClass('scrolled') && $navbar.offset().top <= 50) {
-      $navbar.removeClass('scrolled');
-      $('.navbar-brand').animate({
-        'font-size': '2em'
-      });
-    }
-  });
   $('#simple-search-form').submit(function(e){
     if ($.trim($('#simple-search-input').val()) === '') {
       e.preventDefault();
